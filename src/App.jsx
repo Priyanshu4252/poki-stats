3c0a4983ddf503674dacdb61b925ee6de638f7ae
Priyanshu Sharma <priyanshusharma4850@gmail.com>
2026-03-03 18:07:06 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import Cards from './Cards.jsx'
 import Header from './Header.jsx'
+import Bar from './search_bar.jsx';
+import React, { useState } from "react";
 
 function App() {
 	const ids = Array.from({length:1025},function(_,i){
 		return i+1;
 	})
+
+	const [searchValue, setSearchValue] = useState("");
+
+	function handleSearch(value) {
+    	setSearchValue(value);
+	}
+	
 	return(
 		<>
 			<Header/>
+			<Bar onSearch={handleSearch}/>
 			<div id='grid'>
-				{ids.map(function(id) {
-					return <Cards key={id} i={id} />;
-				})}
+				{searchValue === "" ? ids.map(function(id) {
+						return <Cards key={id} i={id} />;
+					})  
+					: <Cards key={searchValue} i={searchValue} />
+				}
 			</div>
 		</>
   	)
---
+++ src/search_bar.jsx
@@ -0,0 +1,29 @@
+import React,{ useState} from "react";
+
+function Bar(props){
+
+    const [input,setInput]= useState("");
+
+    function handlechange(event){
+        setInput(event.target.value);
+    }
+    function handleclick(event){
+        if(event.key === "Enter"){
+            props.onSearch(input.toLowerCase().trim());
+        }
+    }
+    return (
+        <div id="input_container">
+            <input 
+                type="text" 
+                placeholder="Search pokemon by id (1-1025) or by name.." 
+                value={input} 
+                onChange ={handlechange} 
+                onKeyDown={handleclick} 
+                id="input" />
+            <button id="search_button" onClick={handleclick} >Search</button>
+        </div>
+    );
+}
+
+export default Bar
